1be23922d088a6220aee8b61ec4d935d490cb6cd
ogolla23 <leonardogolla23@gmail.com>
2023-11-20 05:48:10 -0800

    update 2.4

--- models/city.py
+++ models/city.py
@@ -8,16 +8,16 @@ from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ The city class, in the database """
     __tablename__ = 'cities'
     name = Column(
         String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    ) if os.environ.get('HBNB_TYPE_STORAGE') == 'db' else ''
     state_id = Column(
         String(60), ForeignKey('states.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    ) if os.environ.get('HBNB_TYPE_STORAGE') == 'db' else ''
     places = relationship(
         'Place',
         cascade='all, delete, delete-orphan',
         backref='cities'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    ) if os.environ.get('HBNB_TYPE_STORAGE') == 'db' else None
